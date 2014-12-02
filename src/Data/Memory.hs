c275e611f763f3ba4ceafd1d245f1e6087696bf5
bairyn <byron@byronjohnson.net>
2014-12-02 10:48:54 -0800

    Fix warnings in Data.Memory.

--- src/Data/Memory.hs
+++ src/Data/Memory.hs
@@ -25,22 +25,21 @@ module Data.Memory ( Memory
 import Prelude hiding (null)
 import Data.ByteString.Internal (c2w, w2c)
 import Data.Dequeue
-import Data.Maybe (fromJust)
 import Data.Word (Word8)
 
 type Cell              = Word8
 type InternalContainer = BankersDequeue Cell  -- Back is right end; Front is left end
 
-data Memory = ID { id_p :: InternalContainer
-                 , id_i :: InternalContainer
-                 , id_d :: InternalContainer
+data Memory = ID { _id_p :: InternalContainer
+                 , _id_i :: InternalContainer
+                 , id_d  :: InternalContainer
                  }
-            | DI { di_p :: InternalContainer
-                 , di_d :: InternalContainer
-                 , di_i :: InternalContainer
+            | DI { _di_p :: InternalContainer
+                 , di_d  :: InternalContainer
+                 , _di_i :: InternalContainer
                  }
-            | E  { e_p  :: InternalContainer
-                 , e_e  :: InternalContainer
+            | E  { _e_p  :: InternalContainer
+                 , e_e   :: InternalContainer
                  }
 
 initialMemory :: [Cell] -> Memory
@@ -156,39 +155,39 @@ moveDataPointerToInstructionPointer mem@(DI {}) = moveDataPointerToInstructionPo
 moveDataPointerToInstructionPointer mem@(E  {}) = mem
 
 getInstruction :: Memory -> Cell
-getInstruction (ID p i d) =
+getInstruction (ID p  i _d) =
     case popFront i of
         ((Just cell), _) -> cell
         ((Nothing),   _) -> getInstruction $ E p i
-getInstruction (DI p d i) =
+getInstruction (DI _p _d i) =
     case popFront i of
         ((Just cell), _) -> cell
         ((Nothing),   _) -> defaultCell
-getInstruction (E  p e) =
+getInstruction (E  _p e)    =
     case popFront e of
         ((Just cell), _) -> cell
         ((Nothing),   _) -> defaultCell
 
 getData :: Memory -> Cell
-getData (ID p i d) =
+getData (ID _p _i d) =
     case popFront d of
         ((Just cell), _) -> cell
         ((Nothing),   _) -> defaultCell
-getData (DI p d i) =
+getData (DI p  d  i) =
     case popFront d of
         ((Just cell), _) -> cell
         ((Nothing),   _) -> getData $ E p i
-getData (E  p e) =
+getData (E  _p    e) =
     case popFront e of
         ((Just cell), _) -> cell
         ((Nothing),   _) -> defaultCell
 
 setData :: Cell -> Memory -> Memory
-setData cell mem@(ID p i d) = mem{id_d = flip pushFront cell . snd $ popFront d}
-setData cell mem@(DI p d i)
+setData cell mem@(ID _p _i d) = mem{id_d = flip pushFront cell . snd $ popFront d}
+setData cell mem@(DI p  d  i)
     | null d    = setData cell $ E p i
     | otherwise = mem{di_d  = flip pushFront cell . snd $ popFront d}
-setData cell mem@(E  p e)   = mem{e_e  = flip pushFront cell . snd $ popFront e}
+setData cell mem@(E  _p e)   = mem{e_e  = flip pushFront cell . snd $ popFront e}
 
 modifyData :: (Cell -> Cell) -> Memory -> Memory
 modifyData f mem = flip setData mem . f . getData $ mem
